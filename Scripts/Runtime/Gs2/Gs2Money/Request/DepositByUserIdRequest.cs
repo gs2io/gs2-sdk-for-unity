508acf2e5057a057c6af636589192d9545a9ac75
niwa_kazutomo <niwa_kazutomo@gs2.io>
2019-10-07 21:41:56 +0900

    float 型のフィールドのJSONパースがうまく動かないケースがあったのを修正

--- Scripts/Runtime/Gs2/Gs2Money/Request/DepositByUserIdRequest.cs
+++ Scripts/Runtime/Gs2/Gs2Money/Request/DepositByUserIdRequest.cs
@@ -125,7 +125,7 @@ namespace Gs2.Gs2Money.Request
                 namespaceName = data.Keys.Contains("namespaceName") && data["namespaceName"] != null ? (string) data["namespaceName"] : null,
                 userId = data.Keys.Contains("userId") && data["userId"] != null ? (string) data["userId"] : null,
                 slot = data.Keys.Contains("slot") && data["slot"] != null ? (int?) data["slot"] : null,
-                price = data.Keys.Contains("price") && data["price"] != null ? (float?) data["price"] : null,
+                price = data.Keys.Contains("price") && data["price"] != null ? data["price"].IsInt ? (float?)(int?) data["price"] : (float?) data["price"] : null,
                 count = data.Keys.Contains("count") && data["count"] != null ? (int?) data["count"] : null,
                 duplicationAvoider = data.Keys.Contains("duplicationAvoider") && data["duplicationAvoider"] != null ? (string) data["duplicationAvoider"] : null,
             };
