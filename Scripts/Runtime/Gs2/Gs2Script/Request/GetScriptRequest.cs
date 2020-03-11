f418dc15e2508d6d766de6c03b3b4c795b760eed
gs2-ci <ci@gs2.io>
2020-03-11 15:22:09 +0000

    [gs2-script] Service/test seeds modified
    gs2-script: 594840ef5f34245f316dd70a85ca4f0f949708e6
    gs2-sdk-gen: a379474bb6dfb52749b7cf7fa9f00ec34ba5d992
    gs2-test-gen: 9ad1f053bf32e80eb3cb1da3b382f83168d7ef89

--- Scripts/Runtime/Gs2/Gs2Script/Request/GetScriptRequest.cs
+++ Scripts/Runtime/Gs2/Gs2Script/Request/GetScriptRequest.cs
@@ -25,13 +25,11 @@ using UnityEngine.Scripting;
 namespace Gs2.Gs2Script.Request
 {
 	[Preserve]
-	[System.Serializable]
 	public class GetScriptRequest : Gs2Request<GetScriptRequest>
 	{
 
         /** ネームスペース名 */
-		[UnityEngine.SerializeField]
-        public string namespaceName;
+        public string namespaceName { set; get; }
 
         /**
          * ネームスペース名を設定
@@ -46,8 +44,7 @@ namespace Gs2.Gs2Script.Request
 
 
         /** スクリプト名 */
-		[UnityEngine.SerializeField]
-        public string scriptName;
+        public string scriptName { set; get; }
 
         /**
          * スクリプト名を設定
