908cfedd6c972cfb63b322293b973db572fd7884
gs2-ci <ci@gs2.io>
2020-03-12 05:07:38 +0000

    [gs2-identifier] Service/test seeds modified
    gs2-identifier: c9c1fea02c1c372bc2d6341289672cac316138ae
    gs2-sdk-gen: a379474bb6dfb52749b7cf7fa9f00ec34ba5d992
    gs2-test-gen: 9ad1f053bf32e80eb3cb1da3b382f83168d7ef89

--- Scripts/Runtime/Gs2/Gs2Identifier/Request/UpdateUserRequest.cs
+++ Scripts/Runtime/Gs2/Gs2Identifier/Request/UpdateUserRequest.cs
@@ -25,13 +25,11 @@ using UnityEngine.Scripting;
 namespace Gs2.Gs2Identifier.Request
 {
 	[Preserve]
-	[System.Serializable]
 	public class UpdateUserRequest : Gs2Request<UpdateUserRequest>
 	{
 
         /** ユーザー名 */
-		[UnityEngine.SerializeField]
-        public string userName;
+        public string userName { set; get; }
 
         /**
          * ユーザー名を設定
@@ -46,8 +44,7 @@ namespace Gs2.Gs2Identifier.Request
 
 
         /** ユーザの説明 */
-		[UnityEngine.SerializeField]
-        public string description;
+        public string description { set; get; }
 
         /**
          * ユーザの説明を設定
