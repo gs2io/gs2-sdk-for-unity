24d945c53036209505462826596fdd919c06faa0
Kazutomo Niwa <niwa_kazutomo@gs2.io>
2020-03-13 02:39:31 +0900

    プレイヤー全体にメッセージを届けられるように

--- Scripts/Runtime/Gs2/Gs2Inbox/Request/SendMessageByUserIdRequest.cs
+++ Scripts/Runtime/Gs2/Gs2Inbox/Request/SendMessageByUserIdRequest.cs
@@ -29,14 +29,14 @@ namespace Gs2.Gs2Inbox.Request
 	public class SendMessageByUserIdRequest : Gs2Request<SendMessageByUserIdRequest>
 	{
 
-        /** プレゼントボックス名 */
+        /** ネームスペース名 */
 		[UnityEngine.SerializeField]
         public string namespaceName;
 
         /**
-         * プレゼントボックス名を設定
+         * ネームスペース名を設定
          *
-         * @param namespaceName プレゼントボックス名
+         * @param namespaceName ネームスペース名
          * @return this
          */
         public SendMessageByUserIdRequest WithNamespaceName(string namespaceName) {
