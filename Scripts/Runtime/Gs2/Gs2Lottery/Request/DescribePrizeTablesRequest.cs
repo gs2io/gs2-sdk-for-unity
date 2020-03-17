c827f7715c48acf24ad85bbecaa09caddee1ba66
gs2-ci <ci@gs2.io>
2020-03-17 07:02:33 +0000

    [gs2-lottery] Service/test seeds modified
    gs2-lottery: 48488f7abd60634df57cf89bebb21218e40ab2de
    gs2-sdk-gen: 0934fecee188aae4e52c2eb3420b4c9fb8c74231
    gs2-test-gen: ed16153c9ac5a073a5ffbbd499fb2589fff45ece

--- Scripts/Runtime/Gs2/Gs2Lottery/Request/DescribePrizeTablesRequest.cs
+++ Scripts/Runtime/Gs2/Gs2Lottery/Request/DescribePrizeTablesRequest.cs
@@ -25,11 +25,13 @@ using UnityEngine.Scripting;
 namespace Gs2.Gs2Lottery.Request
 {
 	[Preserve]
+	[System.Serializable]
 	public class DescribePrizeTablesRequest : Gs2Request<DescribePrizeTablesRequest>
 	{
 
         /** ネームスペース名 */
-        public string namespaceName { set; get; }
+		[UnityEngine.SerializeField]
+        public string namespaceName;
 
         /**
          * ネームスペース名を設定
