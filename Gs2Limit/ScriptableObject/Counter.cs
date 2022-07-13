38a2e8726e22711d1797a324c8ff4c87bf7a2892
gs2-ci <ci@gs2.io>
2022-07-13 11:22:10 +0000

    725f16457f2603cf1ef7b920e33d2e198bc0e002

---
+++ Gs2Limit/ScriptableObject/Counter.cs
@@ -0,0 +1,36 @@
+#if UNITY_INCLUDE_TESTS
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace Gs2.Unity.Gs2Limit.ScriptableObject
+{
+    [CreateAssetMenu(fileName = "Counter", menuName = "Game Server Services/Gs2Limit/Counter")]
+    public class Counter : UnityEngine.ScriptableObject
+    {
+        public Limit limit;
+        public string counterName;
+        
+#if UNITY_INCLUDE_TESTS
+        public static Limit Load(
+            string assetPath
+        )
+        {
+            return Instantiate(
+                AssetDatabase.LoadAssetAtPath<Limit>(assetPath));
+        }
+#endif
+        
+        public static Counter New(
+            Limit limit,
+            string counterName
+        )
+        {
+            var instance = CreateInstance<Counter>();
+            instance.name = "Runtime";
+            instance.limit = limit;
+            instance.counterName = counterName;
+            return instance;
+        }
+    }
+}
