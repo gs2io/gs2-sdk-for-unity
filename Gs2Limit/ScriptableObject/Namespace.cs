a130c83ee04a88a937a69383b953ab8d836c987e
gs2-ci <ci@gs2.io>
2022-09-06 13:03:55 +0000

    a36b0267bccecf0e1c7f8cfc7bf4a64669d50278

--- Gs2Limit/ScriptableObject/Namespace.cs
+++ Gs2Limit/ScriptableObject/Namespace.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace Gs2.Unity.Gs2Limit.ScriptableObject
 {
-    [CreateAssetMenu(fileName = "Namespace", menuName = "Game Server Services/Gs2Namespace/Namespace")]
+    [CreateAssetMenu(fileName = "Namespace", menuName = "Game Server Services/Gs2Limit/Namespace")]
     public class Namespace : UnityEngine.ScriptableObject
     {
         public string namespaceName;
