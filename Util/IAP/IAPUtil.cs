48dfcc2807032b5b424393dcc93b19114dc0500d
gs2-ci <ci@gs2.io>
2026-09-08 17:46:08 +0000

    75bdb8808a8aeebd8084961edb8448d780a26796

--- Util/IAP/IAPUtil.cs
+++ Util/IAP/IAPUtil.cs
@@ -29,7 +29,7 @@ namespace Gs2.Unity.Util
         Purchased,
         PurchaseFailed,
     }
-    
+
     public class PurchaseParameters
     {
         public string receipt;
@@ -42,7 +42,7 @@ namespace Gs2.Unity.Util
         public PendingOrder product => order;
 #endif
     }
-    
+
 #if GS2_IAP_5_0_0_OR_NEWER
     public static class StoreControllerExt {
         public static void ConfirmPendingPurchase(this StoreController self, PendingOrder order) {
@@ -50,7 +50,7 @@ namespace Gs2.Unity.Util
         }
     }
 #endif
-    
+
     public class IAPUtil
     {
 #if !GS2_IAP_5_0_0_OR_NEWER
@@ -60,7 +60,7 @@ namespace Gs2.Unity.Util
         private Status _status = Status.None;
         private Gs2Exception _exception;
         private string _receipt;
- 
+
         public IEnumerator Buy(
             UnityAction<AsyncResult<PurchaseParameters>> callback,
             string contentsId,
@@ -73,14 +73,14 @@ namespace Gs2.Unity.Util
                 future.Error
             ));
         }
-        
+
         public Gs2Future<PurchaseParameters> BuyFuture(
             string contentsId,
             ProductType productType = ProductType.Consumable
         )
         {
             IEnumerator Impl(Gs2Future<PurchaseParameters> result) {
-                
+
                 if (_status != Status.None)
                 {
                     result.OnError(
@@ -96,7 +96,7 @@ namespace Gs2.Unity.Util
                 _exception = null;
                 _receipt = null;
                 _status = Status.Initializing;
-            
+
 #if !GS2_IAP_5_0_0_OR_NEWER
                 var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
                 builder.AddProduct(contentsId, productType);
@@ -110,14 +110,14 @@ namespace Gs2.Unity.Util
                     result.OnError(_exception);
                     yield break;
                 }
-             
+
                 _status = Status.Purchasing;
                 _controller.InitiatePurchase(_controller.products.WithID(contentsId));
                 while (_status == Status.Purchasing)
                 {
                     yield return new WaitForSeconds(0.1f);
                 }
-            
+
                 result.OnComplete(
                     new PurchaseParameters {
                         receipt = _receipt,
@@ -126,58 +126,75 @@ namespace Gs2.Unity.Util
                     }
                 );
 #else
-                _receipt = null;
-                _exception = null;
-            
-                var controller = UnityIAPServices.StoreController();
-                controller.Connect().Start();
-            
-                _status = Status.Purchasing;
+                using (var session = new Gs2StoreSession(this, contentsId)) {
 
-                PendingOrder order = null;
-                controller.FetchProducts(new List<ProductDefinition>
-                {
-                    new(contentsId, productType),
-                });
-                
-                void OnPurchasePending(PendingOrder pending) {
-                    _receipt = pending.Info.Receipt;
-                    order = pending;
-                    _status = Status.Purchased;
-                }
-                
-                void OnPurchaseFailed(FailedOrder failed) {
-                    _exception = new BadGatewayException(failed.ToString());
-                    _status = Status.PurchaseFailed;
-                }
+                    var connectTask = session.Controller.Connect();
+                    while (!connectTask.IsCompleted) {
+                        yield return null;
+                    }
+                    if (connectTask.IsFaulted) {
+                        _status = Status.InitializeFailed;
+                        result.OnError(
+                            new BadGatewayException(
+                                connectTask.Exception?.GetBaseException().Message ?? "connect failed"
+                            )
+                        );
+                        yield break;
+                    }
+                    _status = Status.Initialized;
 
-                try {
-                    controller.OnPurchasePending += OnPurchasePending;
-                    controller.OnPurchaseFailed += OnPurchaseFailed;
-
-                    while (_status == Status.Purchasing) {
+                    session.Controller.FetchProducts(new List<ProductDefinition>
+                    {
+                        new(contentsId, productType),
+                    });
+                    while (!session.ProductsFetched) {
                         yield return new WaitForSeconds(0.1f);
                     }
-                } finally {
-                    controller.OnPurchasePending -= OnPurchasePending;
-                    controller.OnPurchaseFailed -= OnPurchaseFailed;
-                }
-
-                result.OnComplete(
-                    new PurchaseParameters {
-                        receipt = _receipt,
-                        controller = controller,
-                        order = order,
+                    if (session.Product == null) {
+                        _status = Status.PurchaseFailed;
+                        result.OnError(
+                            _exception ?? new BadGatewayException("product not found: " + contentsId)
+                        );
+                        yield break;
                     }
-                );
+
+                    session.Controller.FetchPurchases();
+                    while (!session.PurchasesFetched) {
+                        yield return new WaitForSeconds(0.1f);
+                    }
+
+                    if (session.Order == null) {
+                        _status = Status.Purchasing;
+                        session.Controller.PurchaseProduct(session.Product);
+                        while (_status == Status.Purchasing) {
+                            yield return new WaitForSeconds(0.1f);
+                        }
+                    }
+
+                    if (session.Order == null) {
+                        _status = Status.PurchaseFailed;
+                        result.OnError(
+                            _exception ?? new BadGatewayException("purchase failed: " + contentsId)
+                        );
+                        yield break;
+                    }
+
+                    result.OnComplete(
+                        new PurchaseParameters {
+                            receipt = _receipt,
+                            controller = session.Controller,
+                            order = session.Order,
+                        }
+                    );
+                }
 #endif
             }
 
             return new Gs2InlineFuture<PurchaseParameters>(Impl);
         }
-        
+
 #if GS2_ENABLE_UNITASK
-        
+
         public async UniTask<PurchaseParameters> BuyAsync(
             string contentsId,
             ProductType productType = ProductType.Consumable
@@ -190,7 +207,7 @@ namespace Gs2.Unity.Util
             _exception = null;
             _receipt = null;
             _status = Status.Initializing;
-            
+
 #if !GS2_IAP_5_0_0_OR_NEWER
             var builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
             builder.AddProduct(contentsId, productType);
@@ -203,7 +220,7 @@ namespace Gs2.Unity.Util
             {
                 throw this._exception ?? new Exception();
             }
-             
+
             _status = Status.Purchasing;
 
             foreach (var product in _controller.products.all ?? Array.Empty<Product>()) {
@@ -216,7 +233,7 @@ namespace Gs2.Unity.Util
                     };
                 }
             }
-            
+
             _controller.InitiatePurchase(_controller.products.WithID(contentsId));
             while (_status == Status.Purchasing)
             {
@@ -230,49 +247,54 @@ namespace Gs2.Unity.Util
                 product = _controller.products.WithID(contentsId),
             };
 #else
-            _receipt = null;
-            _exception = null;
-            
-            var controller = UnityIAPServices.StoreController();
-            await controller.Connect();
-            
-            _status = Status.Purchasing;
+            using (var session = new Gs2StoreSession(this, contentsId)) {
 
-            PendingOrder order = null;
-            controller.FetchProducts(new List<ProductDefinition>
-            {
-                new(contentsId, productType),
-            });
-            
-            void OnPurchasePending(PendingOrder pending) {
-                _receipt = pending.Info.Receipt;
-                order = pending;
-                _status = Status.Purchased;
-            }
-                
-            void OnPurchaseFailed(FailedOrder failed) {
-                _exception = new BadGatewayException(failed.ToString());
-                _status = Status.PurchaseFailed;
-            }
+                try {
+                    await session.Controller.Connect();
+                }
+                catch (Exception e) {
+                    _status = Status.InitializeFailed;
+                    throw new BadGatewayException(e.Message);
+                }
+                _status = Status.Initialized;
 
-            controller.OnPurchasePending += OnPurchasePending;
-            controller.OnPurchaseFailed += OnPurchaseFailed;
-
-            try {
-                while (_status == Status.Purchasing) {
+                session.Controller.FetchProducts(new List<ProductDefinition>
+                {
+                    new(contentsId, productType),
+                });
+                while (!session.ProductsFetched) {
                     await UniTask.Delay(TimeSpan.FromMilliseconds(100));
                 }
-            } finally {
-                controller.OnPurchasePending -= OnPurchasePending;
-                controller.OnPurchaseFailed -= OnPurchaseFailed;
-            }
+                if (session.Product == null) {
+                    _status = Status.PurchaseFailed;
+                    throw _exception ?? new BadGatewayException("product not found: " + contentsId);
+                }
 
-            return new PurchaseParameters
-            {
-                receipt = _receipt,
-                controller = controller,
-                order = order,
-            };
+                session.Controller.FetchPurchases();
+                while (!session.PurchasesFetched) {
+                    await UniTask.Delay(TimeSpan.FromMilliseconds(100));
+                }
+
+                if (session.Order == null) {
+                    _status = Status.Purchasing;
+                    session.Controller.PurchaseProduct(session.Product);
+                    while (_status == Status.Purchasing) {
+                        await UniTask.Delay(TimeSpan.FromMilliseconds(100));
+                    }
+                }
+
+                if (session.Order == null) {
+                    _status = Status.PurchaseFailed;
+                    throw _exception ?? new BadGatewayException("purchase failed: " + contentsId);
+                }
+
+                return new PurchaseParameters
+                {
+                    receipt = _receipt,
+                    controller = session.Controller,
+                    order = session.Order,
+                };
+            }
 #endif
         }
 #endif
@@ -281,18 +303,18 @@ namespace Gs2.Unity.Util
         private class Gs2StoreListener : IStoreListener
         {
             private readonly IAPUtil _client;
- 
+
             public Gs2StoreListener(IAPUtil client)
             {
                 _client = client;
             }
- 
+
             public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
             {
                 _client._controller = controller;
                 _client._status = Status.Initialized;
             }
- 
+
 #if GS2_IAP_4_6_0_OR_NEWER
             public void OnInitializeFailed(InitializationFailureReason error, string message)
             {
@@ -307,18 +329,130 @@ namespace Gs2.Unity.Util
                 _client._exception = new BadGatewayException(error.ToString());
                 _client._status = Status.InitializeFailed;
             }
- 
+
             public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
             {
                 _client._receipt = e.purchasedProduct.receipt;
                 _client._status = Status.Purchased;
                 return PurchaseProcessingResult.Pending;
             }
- 
+
             public void OnPurchaseFailed(Product product, PurchaseFailureReason error)
             {
                 _client._exception = new BadGatewayException(error.ToString());
                 _client._status = Status.PurchaseFailed;
+            }
+        }
+#else
+        private class Gs2StoreSession : IDisposable
+        {
+            private readonly IAPUtil _client;
+            private readonly string _contentsId;
+
+            public readonly StoreController Controller;
+
+            public bool ProductsFetched { get; private set; }
+
+            public Product Product { get; private set; }
+
+            public bool PurchasesFetched { get; private set; }
+
+            public PendingOrder Order { get; private set; }
+
+            public Gs2StoreSession(IAPUtil client, string contentsId)
+            {
+                _client = client;
+                _contentsId = contentsId;
+
+                Controller = UnityIAPServices.StoreController();
+
+                Controller.OnProductsFetched += OnProductsFetched;
+                Controller.OnProductsFetchFailed += OnProductsFetchFailed;
+                Controller.OnPurchasesFetched += OnPurchasesFetched;
+                Controller.OnPurchasesFetchFailed += OnPurchasesFetchFailed;
+                Controller.OnPurchasePending += OnPurchasePending;
+                Controller.OnPurchaseDeferred += OnPurchaseDeferred;
+                Controller.OnPurchaseFailed += OnPurchaseFailed;
+            }
+
+            public void Dispose()
+            {
+                Controller.OnProductsFetched -= OnProductsFetched;
+                Controller.OnProductsFetchFailed -= OnProductsFetchFailed;
+                Controller.OnPurchasesFetched -= OnPurchasesFetched;
+                Controller.OnPurchasesFetchFailed -= OnPurchasesFetchFailed;
+                Controller.OnPurchasePending -= OnPurchasePending;
+                Controller.OnPurchaseDeferred -= OnPurchaseDeferred;
+                Controller.OnPurchaseFailed -= OnPurchaseFailed;
+            }
+
+            private void OnProductsFetched(List<Product> products)
+            {
+                Product = products.FirstOrDefault(product => product.definition?.id == _contentsId);
+                ProductsFetched = true;
+            }
+
+            private void OnProductsFetchFailed(ProductFetchFailed failure)
+            {
+                _client._exception = new BadGatewayException(failure.FailureReason);
+                ProductsFetched = true;
+            }
+
+            private void OnPurchasesFetched(Orders orders)
+            {
+                foreach (var pending in orders.PendingOrders) {
+                    OnPurchasePending(pending);
+                }
+                PurchasesFetched = true;
+            }
+
+            private void OnPurchasesFetchFailed(PurchasesFetchFailureDescription failure)
+            {
+                Debug.LogWarning(
+                    "GS2: failed to fetch purchases. " + failure.FailureReason + " " + failure.Message
+                );
+                PurchasesFetched = true;
+            }
+
+            private void OnPurchasePending(PendingOrder order)
+            {
+                if (Order != null || !IsTarget(order)) {
+                    return;
+                }
+                Order = order;
+                _client._receipt = order.Info.Receipt;
+                _client._status = Status.Purchased;
+            }
+
+            private void OnPurchaseDeferred(DeferredOrder order)
+            {
+                if (Order != null || !IsTarget(order)) {
+                    return;
+                }
+                _client._exception = new BadGatewayException(
+                    "purchase deferred: " + _contentsId
+                );
+                _client._status = Status.PurchaseFailed;
+            }
+
+            private void OnPurchaseFailed(FailedOrder order)
+            {
+                if (Order != null) {
+                    return;
+                }
+                _client._exception = new BadGatewayException(
+                    order.FailureReason + " " + order.Details
+                );
+                _client._status = Status.PurchaseFailed;
+            }
+
+            private bool IsTarget(Order order)
+            {
+                var items = order?.CartOrdered?.Items();
+                if (items == null) {
+                    return false;
+                }
+                return items.Any(item => item.Product?.definition?.id == _contentsId);
             }
         }
 #endif
